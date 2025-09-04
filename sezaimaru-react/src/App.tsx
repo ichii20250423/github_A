5a66636362d90ee41933ca72188ace4925576b13
SezaimaruHaruchika <sezaimaru303.kdgo.student@gmail.com>
2025-09-04 09:45:49 +0900

    router

--- sezaimaru-react/src/App.tsx
+++ sezaimaru-react/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -20,17 +20,12 @@ function App() {
         SezaimaruHaruchikaの<br/>
         Reactアプリ
       </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ sezaimaru-react/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
